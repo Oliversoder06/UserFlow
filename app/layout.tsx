0e84f1457ab4c042b8d22050f51b2fe88834b6da
Oliversoder06 <lexrudolf06@gmail.com>
2024-10-19 00:44:53 +0200

    Implement so many things

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 export const metadata: Metadata = {
   title: "UserFlow",
@@ -16,6 +17,9 @@ export default function RootLayout({
       <body
         className={`bg-[#D4D4D4]`}
       >
+        <header>
+          <Navbar />
+        </header>
         {children}
       </body>
     </html>
